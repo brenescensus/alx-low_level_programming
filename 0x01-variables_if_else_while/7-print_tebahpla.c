b00082d2943bcc69d1fc32a1c48a2fbf6d849300
brenescensus <brenescensus99@gmail.com>
2023-03-18 14:12:56 -0700

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: 'lowercase'
+ *Return: always 0
+*/
+int main(void)
+{
+	char lowerCase = 'z';
+
+	while (lowerCase <= 'z')
+	{
+		putchar(lowerCase);
+		lowerCase--;
+	}
+	putchar('\n');
+	return (0);
+}
