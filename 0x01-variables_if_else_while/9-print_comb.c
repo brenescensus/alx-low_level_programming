41f7f1746b38c96a1815c2f5020fcb7ad37fd391
brenescensus <brenescensus99@gmail.com>
2023-03-18 15:04:10 -0700

    tabs numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: 'lowercase'
+ *Return: always 0
+*/
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar('0' + n);
+		putchar(',');
+		putchar('\t');
+	}
+	putchar('\n');
+	return (0);
+}
