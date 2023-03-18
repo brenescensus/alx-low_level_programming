c5bdac29f6eaa6ab8aeb35ce5887d8bdfcdcd08f
brenescensus <brenescensus99@gmail.com>
2023-03-18 13:12:56 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: 'lowercase'
+ *Return: always 0
+*/
+int  main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
