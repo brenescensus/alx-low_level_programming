fc7ef0c852d3dee8dd320ec5a13037b05593bea0
brenescensus <brenescensus99@gmail.com>
2023-03-17 17:56:39 -0700

    characters lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: 'lowercase'
+ *Return: always 0
+*/
+int main(void)
+{
+	putchar(45);
+	putchar('\n');
+	return (0);
+}
